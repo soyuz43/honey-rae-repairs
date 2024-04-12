11b307e46f481825264afaba9d1097c117c40771
William R. Stetar <kebekad673@proton.me>
2024-04-12 15:53:55 -0500

    27 min into the ticket video

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,35 @@
+// src/components/Ticket.jsx
+
+import { useEffect, useState } from "react";
+import { getAllEmployees } from "../services/employeeServices";
+
+
+export const Ticket = ({ticket, name}) => {
+  const [employees, setEmployees] = useState([])
+  const [assignedEmployee, setAssignedEmployee] = useState({})
+
+  useEffect(() =>{
+    getAllEmployees().then((employeesArray) => {
+        setEmployees(employeesArray)
+    })
+  }, [])
+
+  useEffect(() => {
+    const foundEmployee = employees.find(employee => employee.id === ticket.employeeTickets[0]?.employeeId
+    )
+    setAssignedEmployee(foundEmployee)
+  }, [employees, ticket])
+
+  return (
+    <section key={ticket.id} className="ticket">
+      <header className="ticket-info">#{ticket.id}</header>
+      <div>{ticket.description}</div>
+      <footer>
+        <div>
+          <div className="ticket-info">emergency</div>
+          <div>{ticket.emergency ? "yes" : "no"}</div>
+        </div>
+      </footer>
+    </section>
+  );
+};
--- src/services/ticketService.jsx
+++ src/services/ticketService.jsx
@@ -1,4 +1,5 @@
+// src/components/ticketServices.jsx
 export const getAllTickets = () => {
     
-    return fetch(`http://localhost:8088/serviceTickets`).then(res => res.json());
+    return fetch(`http://localhost:8088/serviceTickets?_embed=employeeTickets`).then(res => res.json());
 }
