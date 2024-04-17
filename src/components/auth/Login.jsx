bb8ee1e4e674eb41f455a73711f791e4d925e7ef
William R. Stetar <kebekad673@proton.me>
2024-04-17 11:38:15 -0500

    Added user Auth and logout to NavBar

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import "./Login.css"
-import { getUserByEmail } from "../../services/userService"
+import { getUserByEmail } from "../../services/userServices.jsx"
 
 export const Login = () => {
   const [email, set] = useState("hpassfield7@netvibes.com")
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,1 @@
-export const getUserByEmail = (email) => {
-  return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
-    res.json()
-  )
-}
-
-export const createUser = (customer) => {
-  return fetch("http://localhost:8088/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(customer),
-  }).then((res) => res.json())
-}
+// this page was put here by the script, it is blank
